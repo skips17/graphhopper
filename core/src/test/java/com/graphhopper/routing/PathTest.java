174c38d907e5facd61c4fe8a2b8301e6d46ada86
yassine <skips7121@gmail.com>
2025-11-21 13:26:52 -0500

    Update PathTest.java

--- core/src/test/java/com/graphhopper/routing/PathTest.java
+++ core/src/test/java/com/graphhopper/routing/PathTest.java
@@ -1371,7 +1371,7 @@ public class PathTest {
         p.addEdge(11);
 
         IllegalStateException ex = assertThrows(
-                IllegalStateException.clas,
+                IllegalStateException.class,
                 () -> p.forEveryEdge(new Path.EdgeVisitor() {
                     @Override
                     public void next(EdgeIteratorState edge, int index, int prevId) {}
