3629d7b77a237707d33c0c5cd10c4713d759b3d8
yassine <skips7121@gmail.com>
2025-11-21 01:54:46 -0500

    Update NavigateResourceTest.java

--- navigation/src/test/java/com/graphhopper/navigation/NavigateResourceTest.java
+++ navigation/src/test/java/com/graphhopper/navigation/NavigateResourceTest.java
@@ -1,12 +1,11 @@
 package com.graphhopper.navigation;
+
 import org.junit.jupiter.api.Test;
-import com.graphhopper.GraphHopperConfig;
-import com.graphhopper.util.TranslationMap;
+
 import java.util.Collections;
 import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class NavigateResourceTest {
 
@@ -26,6 +25,5 @@ public class NavigateResourceTest {
         assertEquals(4, bearings.size());
         assertEquals(100, bearings.get(1), .1);
     }
-     
 
 }
